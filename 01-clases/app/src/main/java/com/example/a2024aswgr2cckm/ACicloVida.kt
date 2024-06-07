7eaa49c7fba455dcd291c053b632727d41ca77eb
kevmateo <kevin.cano@epn.edu.ec>
2024-06-07 10:47:40 -0500

    Clase 10

---
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cckm/ACicloVida.kt
@@ -0,0 +1,77 @@
+package com.example.a2024aswgr2cckm
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class ACicloVida : AppCompatActivity() {
+
+    var textoGlobal = ""
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_aciclo_vida)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val botonCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        botonCicloVida.setOnClickListener{
+                irActividad(ACicloVida::class.java)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this,clase)
+        startActivity(intent)
+    }
+}
